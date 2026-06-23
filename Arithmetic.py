84564340c09ee39cc5c760018c0df57d5bac2116
Rishav kumar <65082283+rish3119@users.noreply.github.com>
2026-06-23 22:04:08 +0530

    Add files via upload

---
+++ Arithmetic.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+# Perform arithmetic operations
+print(a + b)   # Sum
+print(a - b)   # Difference
+print(a * b)   # Product
