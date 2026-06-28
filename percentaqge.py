4a14ad273962082e79bec8362adb9d2a9ed6ea86
Rishav kumar <65082283+rish3119@users.noreply.github.com>
2026-06-28 23:22:04 +0530

    Add files via upload

---
+++ percentaqge.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    # Calculate average
+    avg = sum(student_marks[query_name]) / len(student_marks[query_name])
+    
+    # Print with 2 decimal places
+    print(f"{avg:.2f}")
